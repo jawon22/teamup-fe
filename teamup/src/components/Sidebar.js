166aef0e4fb081ab9f243a3e7c385212859f0e6b
smilesyk0323 <smilesyk0323@naver.com>
2023-11-08 21:28:49 +0900

    메인페이지 수정

--- teamup/src/components/Sidebar.js
+++ teamup/src/components/Sidebar.js
@@ -6,6 +6,10 @@ import Emp from './emp';
 import {CgProfile} from "react-icons/cg";
 import {BsFillBellFill} from "react-icons/bs";
 import {RiKakaoTalkFill} from "react-icons/ri";
+import TeamUpLogo from './images/TeamUpLogo.png';
+import 'bootstrap/dist/css/bootstrap.min.css'; // 부트스트랩 CSS 파일 임포트
+import 'bootstrap/dist/js/bootstrap.bundle.min'; // 부트스트랩 JavaScript 파일 임포트
+
 
 const Sidebar = () => {
     // const [user, setUser] = useState({});
@@ -31,13 +35,9 @@ const Sidebar = () => {
                 </div>
 
                 <div className='row'>
-                    <div className='col'>
-                    </div>
-                </div>
-                <div className='row'>
-                    <div className='col'>
+                    <div className='col-6 offset-6'>
                         <div className="sidebar-menu  me-1">
-                            <div className='text-end border  border-radius pe-3' >
+                            <div className='text-end border  border-radius' >
                                 {user}
                                 <button onClick={login}>로그인</button>
                                 <button onClick={logout}>로그아웃</button>
@@ -49,38 +49,18 @@ const Sidebar = () => {
 
             <div className="main-content container-fluid">
                 
-                <nav className="navbar navbar-expand-lg bg-white pe-4 ps-5 ms-5" data-bs-theme="light">
+                <nav className="navbar navbar-expand-lg bg-white pe-5 ms-5" data-bs-theme="light">
                     <div className="container-fluid ">
-                        <NavLink className="navbar-brand">Navbar</NavLink>
+                        <NavLink className="navbar-brand"><img src={TeamUpLogo} alt="TemaUpLog" width={100}/></NavLink>                        
                         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor03" aria-controls="navbarColor03" aria-expanded="false" aria-label="Toggle navigation">
                             <span className="navbar-toggler-icon"></span>
                         </button>
+                                <button class="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight">조직도</button>
                         <div className="collapse navbar-collapse" id="navbarColor03">
                             <ul className="navbar-nav me-auto">
-                                <li className="nav-item">
-                                    <NavLink className="nav-link active">Home
-                                        <span className="visually-hidden">(current)</span>
-                                    </NavLink>
-                                </li>
-                                <li className="nav-item">
-                                    <NavLink className="nav-link">Features</NavLink>
-                                </li>
-                                <li className="nav-item">
-                                    <NavLink className="nav-link">Pricing</NavLink>
-                                </li>
-                                <li className="nav-item">
-                                    <NavLink className="nav-link">About</NavLink>
-                                </li>
-                                <li className="nav-item dropdown">
-                                    <NavLink className="nav-link dropdown-toggle" data-bs-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown</NavLink>
-                                    <div className="dropdown-menu">
-                                        <NavLink className="dropdown-item">Action</NavLink>
-                                        <NavLink className="dropdown-item">Another action</NavLink>
-                                        <NavLink className="dropdown-item">Something else here</NavLink>
-                                        <div className="dropdown-divider"></div>
-                                        <NavLink className="dropdown-item">Separated link</NavLink>
-                                    </div>
-                                </li>
+
+
+
                             </ul>
                             <div className="d-flex">
                                 <RiKakaoTalkFill className="text-primary ms-2 mt-1"size="40"/>
@@ -90,38 +70,29 @@ const Sidebar = () => {
                         </div>
                     </div>
                 </nav>
-                {/* 본문 */}
+                    {/* 본문 */}
 
-        
+                {/* offcanvas  */}
+                <div className='row'><div className='col-10 offset-1'>   
+                    <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
+                    <div class="offcanvas-header">
+                            <h5 class="offcanvas-title" id="offcanvasRightLabel">조직도</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                        </div>
+                        <div class="offcanvas-body">
+                            조직도 자리 
+                        </div>
+                    </div>
+                </div></div>
 
-
-                <Emp/>
-
-
-
-
-
-
-                <h1>Test</h1>
-                <input className='form-control' />
-                <button classNames="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight">Toggle right offcanvas</button>
+                <div className='row'><div className='col ms-5'>
+                    <Emp/>
+                </div></div>
             </div>
 
 
 
 
-            {/* offcanvas  */}
-            <button className="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasTop" aria-controls="offcanvasTop">Toggle top offcanvas</button>
-
-                <div className="offcanvas offcanvas-top" tabindex="-1" id="offcanvasTop" aria-labelledby="offcanvasTopLabel">
-                <div className="offcanvas-header">
-                    <h5 className="offcanvas-title" id="offcanvasTopLabel">Offcanvas top</h5>
-                    <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-                </div>
-                <div className="offcanvas-body">
-                    ...
-                </div>
-                </div>
         </div>
 
     );
