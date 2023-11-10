693e33026c38ed983a8dcc01867c8e5c21f2b703
jawon22 <winwkdnjs@naver.com>
2023-11-10 17:59:53 +0900

    전자결재 목록 및 기안상신 페이지 연결 구현중

--- teamup/src/App.js
+++ teamup/src/App.js
@@ -1,8 +1,10 @@
+import { Route, Routes } from 'react-router';
 import './App.css';
 import Sidebar from './components/Sidebar';
 import "./components/Sidebar";
-import Emp from './components/emp';
 import "./components/styles.css";
+import ApproveList from "./components/ApproveList";
+import ApproveWrite from "./components/ApproveWrite";
 
 
 function App() {
@@ -11,6 +13,16 @@ function App() {
         <div className=' main-content container-fluid '>
         <Sidebar />
         {/* 메인 화면 */}
+
+        <div>
+          <Routes>
+            {/* 결재라우터 */}
+            <Route path="/approveList" element={<ApproveList/>}></Route> 
+            <Route path="/approveWrite" element={<ApproveWrite/>}></Route>
+          </Routes>
+
+        </div>
+      
 
         </div>
   
---
+++ teamup/src/components/ApproveList.js
@@ -0,0 +1,40 @@
+import { useLocation } from 'react-router-dom'
+import { NavLink } from 'react-router-dom';
+import axios from "axios";
+import { Modal } from "bootstrap";
+
+const ApproveList = ()=>{
+    const location = useLocation();
+
+    return(
+        <div classNameName="container-fluid">
+            <div className="row">
+                <div className="col-md-8 offset-md-2">
+                    <div className="text-end my-3" >
+                        <NavLink className={`nav-link ${location.pathname === '/approveWrite' ? 'active' : ''}`} to="/approveWrite">
+                            <button className="btn btn-info">기안 상신 작성</button>
+                        </NavLink>
+                    </div>
+
+                    <div className="btn-group text-start" role="group" aria-label="Basic radio toggle button group">
+                        <input type="radio" className="btn-check" name="btnradio" id="btnradio1"/>
+                        <label className="btn btn-outline-primary" for="btnradio1">수신</label>
+                        <input type="radio" className="btn-check" name="btnradio" id="btnradio2"/>
+                        <label className="btn btn-outline-primary" for="btnradio2">발신</label>
+                        <input type="radio" className="btn-check" name="btnradio" id="btnradio3"/>
+                        <label className="btn btn-outline-primary" for="btnradio3">완료</label>
+                        <input type="radio" className="btn-check" name="btnradio" id="btnradio4"/>
+                        <label className="btn btn-outline-primary" for="btnradio4">참조</label>
+                    </div>
+
+                    <div>
+
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ApproveList;
