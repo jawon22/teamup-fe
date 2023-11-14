b920e9beb3e892a8463849136987be90713a4a01
Bokman0201 <jeonghs24@nate.com>
2023-11-14 18:09:30 +0900

    회원 관리 구역 생성중

--- teamup/src/components/detpInsert.js
+++ teamup/src/components/detpInsert.js
@@ -1,14 +1,18 @@
 import { useRecoilState } from "recoil";
 import { companyState, userState } from "../recoil";
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import { Modal } from "bootstrap";
 
 const DeptInsert = () => {
+    //세션스토리지 아이디만 저장
+
     const [comId] = useRecoilState(companyState);
     const [deptList, setDeptList] = useState([]);
 
-    const [newData, setNewData]= useState();
-///그냥 모달을 여는거로 합시다! 못해먹겠습니다!
+
+    const [newData, setNewData] = useState();
+    ///그냥 모달을 여는거로 합시다! 못해먹겠습니다!
     useEffect(() => {
         console.log(comId)
         loadDetpList();
@@ -28,7 +32,7 @@ const DeptInsert = () => {
 
 
     };
-
+    //-----부서 추가 버튼을 누르면 input창이 보이게
     const deptInsert = () => {
         //추가할 부분 같은 이름의 부서가 있을 경우 등록이 안되게 해야함
         axios({
@@ -37,10 +41,11 @@ const DeptInsert = () => {
             data: {
                 deptName: dept.deptName,
                 comId: comId
-              }
+            }
         }).then(response => {
             console.log(response.data)
             alert("성공!")
+            loadDetpList();
         }
         );
 
@@ -55,55 +60,149 @@ const DeptInsert = () => {
 
         }).then((response) => {
             setDeptList(response.data);
+            console.log(response.data)
         });
     };
 
-    const changeName = () => {
+    const updateDept = () => {
+        console.log(dept.deptNo)
+        delete dept.comId;
         axios({
-            url: "",
-            method: "",
-            data: "",
+            url: `http://localhost:8080/dept/update/${dept.deptNo}`,
+            method: "put",
+            data: { deptName: dept.deptName }
 
-        }).then();
+        }).then(response => {
+            console.log(response.data)
+            closeModal();
+            loadDetpList();
+            alert("수정완료")
+        });
 
     };
-
-
-
-    const [status, setStatus] = useState(false);
     //부서명 변경
-    const changeStatus = () => {
-        setStatus(true);
-        console.log(status)
+
+    //--------------모달 열고 닫기 
+    const bsModal = useRef();
+    const bsModal2 = useRef();
+    const openModal = () => {
+        const modal = new Modal(bsModal.current);
+        modal.show();
+    };
+    const closeModal = () => {
+        const modal = Modal.getInstance(bsModal.current);
+        modal.hide();
+
+        // clearProfile();
+    };
+    const openModal2 = () => {
+        const modal = new Modal(bsModal2.current);
+        modal.show();
+    };
+    const closeModal2 = () => {
+        const modal = Modal.getInstance(bsModal2.current);
+        modal.hide();
+
+        // clearProfile();
     };
 
-    //회사의 부서 인서트
+    //초기값 false
+
+    //------------부서명 수정
+    const changeDeptName = (target) => {
+        setDept({ ...target })
+        openModal();
+
+    };
+
+
+    //------------부서 삭제 부서 인원이 없을경우
+    //------------인원 추가 이동(백부터 만들어야함)
+    const addEmp = (target) => {
+        setDept({ ...target })
+        openModal2();
+    };
+    //회사아이디는 세션에ㅐ 있는 값을 가져와서 하고 
+    //dept는 클릭했을때 번호가 생성이 되고 
+
+
+
+    /////-------------직급 등록
+    const [positionData , setPositionData] =useState({
+            comId: comId,
+            empPositionName: "",
+            empPositionOrder: ''
+    });
+    const changePositionData = (e) => {
+        setPositionData({
+            ...positionData,
+            [e.target.name]: e.target.value
+        })
+    };
+
+
+    const addPosition= ()=>{
+
+        axios({
+            url:"http://localhost:8080/empPosition",
+            method:"post",
+            data:positionData
+        }).then(response=>{
+            if(response.data!= null){
+            alert("성공")}
+
+            setPositionData('')
+        });
+    };
+    ///--사원 불러오기
+
+    const cellClick=(target)=>{
+        alert(`셀이 클릭되었습니다! 부서명: ${dept.deptName}`);
+        setDept({ ...target })
+
+
+        axios({
+
+            
+        }).then();
+    
+
+    };
+
+
 
     return (
 
         <>
             <div className="container">
                 <div className="row mt-4">
-                    <div className="col-6 offset-6">
-                        <h1>부서인서트</h1>
+                    <div className="col-10 offset-1">
+                        <h1>부서관리</h1>
                     </div>
                 </div>
 
                 <div className="row mt-4">
-                    <div className="col-6 offset-6">
+                    <div className="col-3 ">
                         부서명 <input className="form-control" name="deptName" onChange={changeInfo} />
                     </div>
-                </div>
-
-                <div className="row mt-4">
-                    <div className="col-6 offset-6">
-                        <button className="btn btn-primary" onClick={deptInsert}>등록</button>
+                    <div className="col-4 mt-4">
+                        <button className="btn btn-primary" onClick={deptInsert}>추가 </button>
+                    </div>
+                    <div className="col-1 ">
+                        정렬<input type="number" className="form-control" name="empPositionOrder" onChange={changePositionData}/>
+                    </div>
+                    <div className="col-3 ">
+                        직급 <input className="form-control" name="empPositionName" onChange={changePositionData} />
+                    </div>
+                    <div className="col-1 mt-4">
+                        <button className="btn btn-primary " onClick={addPosition}>추가 </button>
                     </div>
                 </div>
+
 
                 {/* 추가할 부분 드롭다운 만들어서 부서별 인원 찾기? 안해도 될거 같긴한데 */}
                 <div className="row mt-4">
-                    <div className="col-6 offset-6">
+                    <div className="col ">
                         <tabel className="table table-border">
                             <thead>
                                 <tr>
@@ -115,33 +214,74 @@ const DeptInsert = () => {
                             <tbody>
                                 {deptList.map((dept) => (
                                     <tr key={dept.deptNo}>
-                                        {dept.edit ? (
-                                            <td>
-                                                <input
-                                                    type="text"
-                                                    value={dept.deptName}
-                                                    onChange={(e) => setDept(e.target.value)}
-                                                />
-                                            </td>
-                                        ) : (
-                                            <>
-                                                <td>{dept.deptName}</td>
-                                                <td>{dept.empCount}</td>
-                                                <td>
-                                                    <button
-                                                        className="btn btn-outline-primary me-2"
-                                                        onClick={changeStatus}
-                                                    >
-                                                        수정
-                                                    </button>
-                                                    <button className="btn btn-outline-primary">삭제</button>
-                                                </td>
-                                            </>
-                                        )}
+
+
+                                        <td onClick={e=>cellClick(dept)}>{dept.deptName}</td>
+                                        <td>{dept.empCount}</td>
+                                        <td>
+                                            <buttnon className="btn btn-outline-primary me-2" onClick={e => addEmp(dept)}>사원등록</buttnon>
+                                            <button className="btn btn-outline-primary me-2" onClick={e => changeDeptName(dept)}>수정</button>
+                                            <button className="btn btn-outline-primary">삭제</button>
+                                        </td>
+
+
                                     </tr>
+
                                 ))}
                             </tbody>
                         </tabel>
+                    </div>
+                </div>
+
+
+                <div className="modal fade" ref={bsModal} id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div className="modal-dialog">
+                        <div className="modal-content">
+                            <div className="modal-header">
+                                <h1 className="modal-title fs-5" id="exampleModalLabel">수정</h1>
+                                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            </div>
+                            <div className="modal-body">
+                                <input type="text" className="form-control" name="deptName" onChange={changeInfo} value={dept.deptName} />
+
+                            </div>
+                            <div className="modal-footer">
+                                <button type="button" className="btn btn-secondary" data-bs-dismiss="modal" onClick={closeModal}>Close</button>
+                                <button type="button" className="btn btn-primary" onClick={updateDept}>Save changes</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div className="modal fade" ref={bsModal2} id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div className="modal-dialog">
+                        <div className="modal-content">
+                            <div className="modal-header">
+                                <h1 className="modal-title fs-5" id="exampleModalLabel">등록</h1>
+                                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            </div>
+                            <div className="modal-body">
+
+                                이름<input className="form-control" />
+                                전화번호<input className="form-control" />
+                                pw<input className="form-control" />
+                                직급선택<select className="form-control">
+                                    {/* {deptList.map((dept) => (
+
+                                        // <option value={dept.deptName}>{dept.deptName}</option>
+
+                                    ))} */}
+
+
+                                </select>
+                                email<input className="form-control" />
+                            </div>
+                            <div className="modal-footer">
+                                <button type="button" className="btn btn-secondary" data-bs-dismiss="modal" onClick={closeModal2}>Close</button>
+                                <button type="button" className="btn btn-primary">Save changes</button>
+                            </div>
+                        </div>
                     </div>
                 </div>
 
