d29e1a50b5ecad12b79b0a4835afedef74b49684
newstar95 <byeol12120@gmail.com>
2023-11-17 10:51:10 +0900

    근태 구현 중

--- teamup/src/components/home.js
+++ teamup/src/components/home.js
@@ -5,58 +5,50 @@ import axios from "axios";
 import { userState } from "../recoil";
 
 const Home = () => {
-  //사원정보
   const [user, setUser] = useRecoilState(userState);
   const [empNo, setEmpNo] = useState('');
+  const [isAttendClicked, setIsAttendClicked] = useState(false);
 
-  // 출근 버튼 클릭 여부를 관리하는 상태 추가
-  const [isAttendClicked, setIsAttendClicked] = useState(false); 
+  //등록을 위한 state
+  const [attendList, setAttendList] = useState([]);
+
+  //수정을 위한 state
+  const [attendUpdateList, setAttendUpdateList] = useState([]);
+
   useEffect(() => {
     setEmpNo(user.substring(6));
   }, [user]);
 
-  const [attendList, setAttendList] = useState([]);
-
-  //- 출근하기 버튼을 눌렀을 때
+  //등록 (출근버튼)
   const attendStartClick = () => {
+
+    //버튼이 클릭된 상태일때
     if (isAttendClicked) {
-      // 이미 버튼이 클릭되었으면 더 이상 실행하지 않음
       return;
     }
 
-    //등록 axios
     axios({
       url: `http://localhost:8080/attend/${empNo}`,
       method: 'post',
-      data: {empNo: empNo}
+      data: { empNo: empNo }
     })
-    .then(response => {
-      setAttendList(response.data);
-      // 로컬 스토리지에 데이터 저장
-      localStorage.setItem("attendData", JSON.stringify(response.data));
-      // 출근 버튼 클릭 여부 업데이트
-      setIsAttendClicked(true);
-    })
-    .catch(err => {
-      window.alert("통신 오류가 발생했습니다!");
-    });
-};
+      .then(response => {
+        setAttendList(response.data);
+        setIsAttendClicked(true);
+      })
+      .catch(err => {
+        window.alert("통신 오류가 발생했습니다!");
+      });
+  };
 
-useEffect(() => {
-  
-}, []); // 빈 배열을 전달하여 컴포넌트가 처음 마운트될 때만 실행되도록 함
-
-  //- 퇴근하기 버튼을 눌렀을 때
   const attendEndClick = () => {
     axios({
       url: `http://localhost:8080/attend/${empNo}`,
       method: "patch"
     })
-    .then(response => {
-      setAttendList(response.data);
-      // 로컬 스토리지에 데이터 저장
-      localStorage.setItem("attendData", JSON.stringify(response.data));
-    });
+      .then(response => {
+        setAttendList(response.data);
+      });
   };
 
   const formatDateTime = (dateTimeString) => {
@@ -72,16 +64,6 @@ useEffect(() => {
     return new Date(dateTimeString).toLocaleString("ko-KR", options);
   };
 
-  useEffect(() => {
-    // 페이지가 로드될 때 로컬 스토리지에서 데이터를 불러와서 상태에 업데이트
-    const storedData = JSON.parse(localStorage.getItem("attendData"));
-    if (storedData) {
-      setAttendList(storedData);
-      // 저장된 데이터가 있으면 출근 버튼을 클릭한 것으로 처리
-      setIsAttendClicked(true);
-    }
-  }, []); // 빈 배열을 전달하여 컴포넌트가 처음 마운트될 때만 실행되도록 함
-
   return (
     <div className="container-fluid">
       <div className="row ms-3">
@@ -91,7 +73,7 @@ useEffect(() => {
           <NavLink to="/profileEdit">프로필 수정(마이페이지에 넣을껍니당)</NavLink>{" "}
           <br />
           <NavLink to="/calendar">일정</NavLink>
-          <br/>
+          <br />
           <div className="d-flex">
             <div className="m-1">{attendList.attendStart ? formatDateTime(attendList.attendStart) : "-"}</div>
             <button className="btn btn-primary" onClick={attendStartClick} disabled={isAttendClicked}>
